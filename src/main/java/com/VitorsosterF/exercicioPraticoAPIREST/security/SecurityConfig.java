a6bdecd16dbe615703ef945c4f4100e4620534b0
DaviRodriFv <seuemail@exemplo.com>
2026-03-10 15:37:36 -0300

    feat: Implementei o SecurityFilterChain, e defini as rotas

---
+++ src/main/java/com/VitorsosterF/exercicioPraticoAPIREST/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.VitorsosterF.exercicioPraticoAPIREST.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private ApiKeyFilter apiKeyFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                        .anyRequest().denyAll()
+                )
+
+                .addFilterBefore(apiKeyFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
